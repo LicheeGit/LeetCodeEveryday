ba7437a97486eb3a4d184014baec492b67932cc8
Lichee-119 <licheego@gmail.com>
2020-09-18 19:50:52 +0800

    day 7

--- src/StringtoIntegeratoi.java
+++ src/StringtoIntegeratoi.java
@@ -5,34 +5,47 @@
  */
 public class StringtoIntegeratoi {
     public static void main(String[] args) {
-        String str = "   -42";
+        /*String[] strs = {"    -42 ", "9223372036854775808", "-91283472332", "4193 with words", "42", " ", "+",
+                "-0001", "  * ", " --+5 08", "+-9823","2147483648", "-2147483648", "-2147483647"};
+        for (String str : strs) {
+            System.out.println(myAtoi(str));
+        }*/
+        String str = "-2147483647";
         System.out.println(myAtoi(str));
-        System.out.println('-' >= '0' && '-' <= '9');
     }
 
     public static int myAtoi(String str) {
+        int i = 0;
         long integer = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ' ' && i < str.length()) i++;
-            else if (str.charAt(i) < '0' || str.charAt(i) > '9') {
-                return 0;
-            } else {
+        boolean negetive = false;
+        while (i < str.length() && str.charAt(i) == ' ') {
+            i++;
+        }
+        if (i < str.length() && str.charAt(i) == '-') {
+            negetive = true;
+            i++;
+        } else if (i < str.length() && str.charAt(i) == '+') {
+            i++;
+        }
+        if (i < str.length() && (str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
+            while (i < str.length() && (str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
+                integer *= 10;
                 integer += str.charAt(i) - '0';
                 i++;
-                while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
-                    integer *= 10;
-                    integer += str.charAt(i) - '0';
-                    i++;
+                if(negetive){
+                    if((0-integer)<=Integer.MIN_VALUE){
+                        return Integer.MIN_VALUE;
+                    }
                 }
-                if (integer < Integer.MIN_VALUE) {
-                    return Integer.MIN_VALUE;
+                if(integer>=Integer.MAX_VALUE){
+                    if(!negetive) return Integer.MAX_VALUE;
                 }
-                if (integer > Integer.MAX_VALUE) {
-                    return Integer.MAX_VALUE;
-                }
-                return (int) integer;
             }
+            if(negetive){
+                return 0-(int) integer;
+            }
+            return (int) integer;
         }
-        return (int) integer;
+        return 0;
     }
 }
